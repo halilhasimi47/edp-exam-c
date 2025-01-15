d8b931669296e45398c0b306e6d8070fd90a1830
seyyit_halil_hasimioglu <150920026+halilhasimi47@users.noreply.github.com>
2025-01-15 13:31:12 +0000

    Add class

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 class Event:
     def __init__(self, payload):
         self.payload = payload
+
+class ApplicationSubmittedEvent(Event):
+    name = "application_submitted"
+
+class ApplicationRejectedEvent(Event):
+    name = "application_rejected"
+
+class ApplicationAcceptedEvent(Event):
+    name = "application_accepted"
+
+communication_queue = []
